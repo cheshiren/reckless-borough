8ed54f386d7a0b6147165e2f3f94230ade769524
Vyacheslav Dobranov <vy.dobranov@yandex.ru>
2025-08-09 19:41:08 +0500

    web

---
+++ build/web/assets/index-V7hy53U2.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./index-CNvEljoA.css"])))=>i.map(i=>d[i]);
+import{_ as a}from"./index-DVPOtXbl.js";a(()=>Promise.resolve({}),__vite__mapDeps([0]),import.meta.url);const o={name:"Iosevka Term Slab",fallback:"monospace"};export{o as default};
